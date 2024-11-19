423945eddf2853ec09ef665b8ccf499a45e97cfd
unknown <nikoraisanenbusiness@gmail.com>
2024-11-18 21:49:23 -0800

    restructure frontend directory

--- backend/reader.py
+++ backend/reader.py
@@ -2,6 +2,7 @@ import json
 
 FILEPATH = './temp_db.json'
 
+
 def read_file(path):
     data = None
     with open(path, 'r') as fp:
@@ -11,13 +12,16 @@ def read_file(path):
             data = None
     return data if data else {}
 
+
 def write_file(path, data):
     with open(path, 'w+') as fp:
         json.dump(data, fp, indent=4)
 
+
 def get_user(id):
     data = read_file(FILEPATH)
     return data.get(id)
+
 
 def update_user(id, val):
     data = read_file(FILEPATH)
